b9a9a6e8775ce99fa991e400a000083b50183af1
sa20122424 <saloxiddin201224@gmail.com>
2024-10-03 18:12:58 +0500

    salom

---
+++ main.js
@@ -0,0 +1,144 @@
+let data = {
+  "All": [
+    {
+      "id": 0,
+      "name": "Чизбургер-пицца",
+      "price": 395,
+      "img": "https://s3-alpha-sig.figma.com/img/bcbd/4371/fe81d3a3ec638fb078b2122eed2d7420?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pwgNolS-3ZH6rmy7q2ytWLr~MK2APjhi~q5HzeF1maguT6IygBOB5iVYOxDLDdyrcfoQ9rugJRh1BIpihh23Txnv9wq5CTHa7p4Gf8y4UUULqWdAVmCxkUV7zJVmjDi9YL42vapXnHhqwG-r1CnX5dh7BNsWokG7qbH1N6UXD7jwCFmaxuAQTu630uGuGDj6KEA92JFjGzmNadhbD4wrQHplpCjfsifTr39dt1h41uVHI-nl-0hSxNl23Mp0zS~9qiHiPsa5eRJvGCjFrsD81FqwkXAjGHndteQIC1ntyCfy~51UdBJoj5MuaVKq3RpIpOacopCv8GAMW-BC9hYTwQ__",
+      "number": 1
+    },
+    {
+      "id": 1,
+      "name": "Сырная",
+      "price": 450,
+      "img": "https://s3-alpha-sig.figma.com/img/2997/9ae7/4b2dd8c255833c6c0db221da6a5b3fae?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gfORx5nYYCCBwY7jxy8XO4grrhu~aY3U5MG1XIg26umdCowtf1Eu17xFg44ZryQ73PPKaxL0lgokeYqtCa9hde0QyOsHArQM6EAJ7v9d9OLxWWLrlhs6h6GndHcTIQYJaOvSpvOW093A7ryp4MSBKb3mgHw52zSD~Sx9sEj56-eXu7ByQrpKx8~epRr7GQq~wpECqmLgZh~2xuiC-tleEO7EQw9Z-13a6k8990g0Upi58ZF7vzO12qpejtDRcKK7CuDf2Gcb6TU57qw0rT1b0LLjbZj6hH44zOaeIV7oWm6yBp06VlxF9w6szFDMCmc1kiL~6uJZi6Tfd0fkYWWymQ__",
+      "number": 1
+    },
+    {
+      "id": 2,
+      "name": "Креветки по-азиатски",
+      "price": 290,
+      "img": "https://s3-alpha-sig.figma.com/img/94bc/0f13/3837f2e7162797b5233b5520d2f5ea7c?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pmk92MjS05u~ult53Mn6XxgoBY3zqeiIOTBn~AyixIqkMraAAP1oK2LyyhGTv8G25fOe~wIL-hWqfwicu9WmcBOgcLT18sJkV-aHMPjlHw64GEDMSu~nxW0mPwIFZRfgcP1TQa7~53AE70HYDLsqmEjZWYZ4G~2J22wnQoRC6P2BtUAEffpqMRZpkRGknv9ozq33g~SmXwPHLkWq-aqmluubw1gZPZZm3ZhqBgRfrovUQTkZp7vQfrxCGIPeFOrXeM821QZE4A-t4cg7my6FoAYA7d7sVzPt5~Tfs-WIiYT3-JNSFbUPfYfNDkDd3W3K1IgNLoLxP~gsiimLQjN3bQ__",
+      "number": 1
+    },
+    {
+      "id": 3,
+      "name": "Сырный цыпленок",
+      "price": 385,
+      "img": "https://s3-alpha-sig.figma.com/img/ca9b/e23e/3d44180b3596e4c86e6b3c3bf2fdc984?Expires=1728864000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DwI0xN~vltQgtambBNp0-kq-mfKM7nyC6~rqpKHcMeHr8fWYJZ0IQiwuwFOcghrKFeDDoXQOz9nyTrcjh2nNIV42cUw4XLYivGcWU~FLrOGwIDSxPtreJEWSdRx4Ww-fDKxmdoDY8NpIzNXm-B5M1NUs7zjbsJLjVLByMTmbMfl4oRCWk4oKdu4PSd4rR1k3xcKi4PXxwID4pMVxO661~6aThW6mWAPtMAz9hmtjdDokLK1RBq3Q076hDEc2bF6b3-s5RZq0GSZOXsyC26g9cs6OkWlr0Cvdeyd2udbprGhpgk0O6yDSy3qpDN0EVZjz8e0Lf6Lu7VJ~csN8w4pkzA__",
+      "number": 1
+    }
+  ],
+  "Meat": [
+    {
+      "id": 0,
+      "name": "Чизбургер-пицца",
+      "price": 395,
+      "img": "/data-img/image1.svg",
+      "number": 1
+    },
+    {
+      "id": 1,
+      "name": "Сырный цыпленок",
+      "price": 385,
+      "img": "/data-img/image4.svg",
+      "number": 1
+    }
+  ],
+  "Seafood": [
+    {
+      "id": 0,
+      "name": "Креветки по-азиатски",
+      "price": 290,
+      "img": "/data-img/image3.svg",
+      "number": 1
+    }
+  ],
+  "Spicy": [
+    {
+      "id": 0,
+      "name": "Сырная",
+      "price": 450,
+      "img": "/data-img/image2.svg",
+      "number": 1
+    },
+    {
+      "id": 1,
+      "name": "Креветки по-азиатски",
+      "price": 290,
+      "img": "/data-img/image3.svg",
+      "number": 1
+    }
+  ],
+  "Grill": [
+    {
+      "id": 0,
+      "name": "Креветки по-азиатски",
+      "price": 290,
+      "img": "/data-img/image3.svg",
+      "number": 1
+    }
+  ]
+}
+
+
+export default data
+
+
+
+let product = data.All
+
+console.log(product);
+
+product.forEach(e => {
+
+  let p = document.createElement("p")
+  let body = document.querySelector("body")
+  let conte = document.querySelector(".conteiner")
+  let all_card = document.querySelector(".all-card")
+  let box = document.createElement("box")
+  
+  p.innerHTML = e.price
+  
+  let p1 = document.createElement("p")
+  p1.innerHTML = e.name
+
+  let img = document.createElement("img")
+  img.src = e.img
+
+  let btn = document.createElement("button")
+
+  btn.textContent = "добавить"
+ 
+   btn.style.width = "100px"
+   btn.style.color = "black" 
+
+ 
+   btn.addEventListener("click", () => {
+    const productToSave = {
+      id: e.id,
+      name: e.name,
+      price: e.price,
+      img: e.img,
+      number: e.number
+    };
+
+    const productString = JSON.stringify(productToSave);
+    const key = `product_${productToSave}`;
+     localStorage.setItem(key, productString);
+    
+    
+  });
+    all_card.append(box)
+    body.append(conte)
+  conte.append(all_card)
+  box.append(img,p1,p,btn)
+ 
+
+
+
+});
+
+
+
+
